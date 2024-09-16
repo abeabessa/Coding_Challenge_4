1e4beb8c76828d45061b16b2355774b03180efe8
abeabessa <bbessamarinho@gmail.com>
2024-09-15 22:48:20 -0400

    Create updateStock function

--- cc_4.js
+++ cc_4.js
@@ -6,3 +6,20 @@ const inventory = [
     {name: 'Headphones', price: 100, quantity: 15, lowStockLevel: 5},
     {name: 'Smartwatch', price: 250, quantity: 3, lowStockLevel:1}
 ];
+function displayProductDetails (product) {
+    const stockLevel = product.quantity <= product.lowStockLevel ? " Low Stock " : " In Stock ";
+    console.log(`Name: ${product.name}`);
+    console.log(`Price: $${product.price}`);
+    console.log(`Quantity: ${product.quantity}`);
+    console.log(`Stock Status: ${stockLevel}`);
+}
+function updateStock (product, unitsSold) {
+    product.quantity -= unitsSold;
+    if (product.quantity <= 0) {
+        console.log(`${product.name} is out of stock.`);
+    } else if ( product.quantity <= product.lowStockLevel) {
+        console.log (`$product.name} is low in stock.`);
+    } else {
+        console.log (`${product.name} stock updated. ${product.quantity} units remaining.`);
+    }
+    }
